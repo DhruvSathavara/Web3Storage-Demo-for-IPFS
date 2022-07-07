7aeee52651e8e8660582c8def576f668ee51cad0
Dhruv Sathavara <dhruvsathavara7228@gmail.com>
2022-07-07 14:56:23 +0530

    web3-storagefor-IPFS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [fileupload, setFileupload] = useState();
 
   async function Web3StorageEx(e) {
-    let file = e.target.files[0];
+    // let file = e.target.files[0];
     let API_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDRCZTZkRUQzRmFmMTNDNWE4RTg3ODliODYxNjZBNkZiQkRFRjlDNTQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTY1ODk4MzM1NzcsIm5hbWUiOiJkaHJ1dldlYjNTdG9yYWdlIn0.6yD0SeKAZVw4GLu2T0EadB47KyIPTXk-LbS-SDCSbEE'
     const client = new Web3Storage({ token: API_TOKEN })
 
@@ -17,33 +17,44 @@ export default function App() {
     const rootCid = await client.put(fileInput.files)
     console.log(rootCid, "root content id...")
 
-    // const URL =  `https://ipfs.infura.io/ipfs/${rootCid}`
-    // console.log(URL);
-
-    setFileupload(rootCid);
+    // setFileupload(rootCid);
 
     // Get info on the Filecoin deals that the CID is stored in
     const info = await client.status(rootCid)
 
+    // const metadata  = JSON.stringify(client.put {
+    //   name: 'dhruv',
+    //   description: 'This is in IPSF storage via NFT storage !',
+    //   Examine: 'By Dhruv',
+    // })
+
     // Fetch and verify files from web3.storage
     const res = await client.get(rootCid)
     const files = await res.files()
+    // console.log(files);
+
+    let url = URL.createObjectURL(files[0])
+    console.log(url)
+
+    setFileupload(url)
+
+    console.log(files.file.cid);
     for (const file of files) {
       console.log(`${file.cid},${file.name}`)
     }
   }
-    return (
-      <>
-        <div className="App">
-          <h1>Web3 storage</h1>
-          <input type="file" onChange={Web3StorageEx}></input>
-          {
-            fileupload && (
-             <p>Content ID: {fileupload}</p>
-            )
-          }
-        </div>
-      </>
-    )
+  return (
+    <div className="App">
+      <h1>Web3Storage - Example</h1>  
+      <div>
+        <input type="file" id="input" onChange={Web3StorageEx} />
+        {
+       fileupload && (
+        <img src={ fileupload } width="200px" />
+      )
+    }
+    </div>
+  </div>
+  )
   
 }
